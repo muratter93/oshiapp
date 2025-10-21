c8b9cc23f4fb7a2a44d700f5b549c2af52678bdb
LAPTOP-1MCCT0SC\y_yadathu <ngn2449953@stu.o-hara.ac.jp>
2025-10-21 11:14:27 +0900

    (;´･ω･)

--- money/admin.py
+++ money/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Wallet
 
-# Register your models here.
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('member', 'cheer_coin_balance', 'stanning_point_balance')
+    search_fields = ('member__username',)
+    
--- money/models.py
+++ money/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Wallet(models.Model):
+    member = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='wallet',
+        verbose_name='会員'
+    )
+    cheer_coin_balance = models.PositiveIntegerField(default=0)
+    stanning_point_balance = models.PositiveIntegerField(default=0)
+
+    def add_cheer_coins(self, yen_amount):
+        if yen_amount >= 5000:
+            coins = 7000
+        elif yen_amount >= 3000:
+            coins = 3600    
+        elif yen_amount >= 1000:
+            coins = 1000
+        elif yen_amount >= 100:
+            coins = 100    
+        else:
+            coins = yen_amount
+        self.cheer_coin_balance += coins
+        self.stanning_point_balance += coins // 100
+        self.save()
+
+    def __str__(self):
+        return f"{self.member.username} のウォレット"
