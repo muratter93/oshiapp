515808456f06244616b92c2d4709eda4d42b8ea0
LAPTOP-ESGIF8LP\m_ikeda <ngn2449550@stu.o-hara.ac.jp>
2025-10-24 12:28:56 +0900

    ランキング作りました。（途中まで）

--- ranking/views.py
+++ ranking/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+# from animals.models import Animal
 
+# def ranking(request):
+
+#     animals = Animal.objects.all().order_by('-total_point')
+
+#     return render(request, "ranking/ranking.html", {'animals' : animals})
+
+from django.shortcuts import render
+from animals.models import Animal
+ 
 def ranking(request):
-    return render(request, "ranking/ranking.html")
+    animals = Animal.objects.all().order_by('-total_point')
+    return render(request, 'ranking/ranking.html', {'animals': animals})
