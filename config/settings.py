ae89cffe33d660f458dd4542f5fc8e1184880be0
LAPTOP-1MCCT0SC\y_yadathu <ngn2449953@stu.o-hara.ac.jp>
2025-10-30 11:28:25 +0900

    かご表示　byヤダツ

--- config/settings.py
+++ config/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [{
         "django.contrib.auth.context_processors.auth",
         "django.contrib.messages.context_processors.messages",
         "common.context_processors.navbar",
+        'goods.context_processors.cart_total_quantity',  # ←かご表示！
     ]},
 }]
 
